59c180a10ea8fece2ae11de084e66d9e5ab7063b
Rick Ertl <ertl.rick@gmail.com>
2022-03-09 13:55:11 -0600

    single round with hardcoded player's entry

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+function computerPlay () {
+    let plays = ["rock", "paper", "scissors"];
+    let play = plays[Math.floor(Math.random() * plays.length)];
+    return play;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let winner = 0;
+    if ( (playerSelection == "rock" && computerSelection == "scissors") || 
+    (playerSelection == "scissors" && computerSelection == "paper") || 
+    (playerSelection == "paper" && computerSelection == "rock") ) {
+        winner = "Player";
+    } else if ( (playerSelection == "scissors" && computerSelection == "rock") || 
+    (playerSelection == "paper" && computerSelection == "scissors") || 
+    (playerSelection == "rock" && computerSelection == "paper") ) {
+        winner = "Computer";
+    } else {
+        winner = "Tie";
+    }
+    return winner;
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log("Computer: " + computerSelection);
+console.log("Player: " + playerSelection);
+console.log("Winner: " + playRound(playerSelection, computerSelection));
