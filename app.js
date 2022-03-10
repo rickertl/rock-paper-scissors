74e8371c69a6d1e8098b68775f2fae25b81c4204
Rick Ertl <ertl.rick@gmail.com>
2022-03-09 19:31:04 -0600

    handled capitalization better

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
-function computerPlay () {
-    let plays = ["rock", "paper", "scissors"];
-    let play = plays[Math.floor(Math.random() * plays.length)];
+//gets computer's random play
+function computerPlay() {
+    const plays = ["Rock", "Paper", "Scissors"];
+    const play = plays[Math.floor(Math.random() * plays.length)];
     return play;
 }
 
-function playRound(playerSelection, computerSelection) {
-    let winner = 0;
-    if ( (playerSelection == "rock" && computerSelection == "scissors") || 
-    (playerSelection == "scissors" && computerSelection == "paper") || 
-    (playerSelection == "paper" && computerSelection == "rock") ) {
-        winner = "Player";
-    } else if ( (playerSelection == "scissors" && computerSelection == "rock") || 
-    (playerSelection == "paper" && computerSelection == "scissors") || 
-    (playerSelection == "rock" && computerSelection == "paper") ) {
-        winner = "Computer";
-    } else {
-        winner = "Tie";
-    }
-    return winner;
+//gets player's inputted play
+function playerPlay() {
+    const play = prompt("What's your play?").toLowerCase();
+    return play.charAt(0).toUpperCase() + play.slice(1);
 }
 
-const playerSelection = "rock";
+//plays a round and determines winner or tie
+function playRound(playerSelection, computerSelection) {
+    let result = 0;
+    if ( (playerSelection == "Rock" && computerSelection == "Scissors") || 
+    (playerSelection == "Scissors" && computerSelection == "Paper") || 
+    (playerSelection == "Paper" && computerSelection == "Rock") ) {
+        result = "You Win! " + playerSelection + " beats " + computerSelection;
+    } else if ( (playerSelection == "Scissors" && computerSelection == "Rock") || 
+    (playerSelection == "Paper" && computerSelection == "Scissors") || 
+    (playerSelection == "Rock" && computerSelection == "Paper") ) {
+        result = "You Lose! " + computerSelection + " beats " + playerSelection;
+    } else {
+        result = "Tie! You both chose " + playerSelection;
+    }
+    return result;
+}
+
+//plays five rounds and keeps score
+function game() {
+    for (let i = 0; i < 5; i++) {
+        // your code here!
+    }
+}
+
 const computerSelection = computerPlay();
+const playerSelection = playerPlay();
 console.log("Computer: " + computerSelection);
 console.log("Player: " + playerSelection);
-console.log("Winner: " + playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
