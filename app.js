598315303d167bc95c2ea32b8847682c176c536a
Rick Ertl <ertl.rick@gmail.com>
2022-03-10 16:38:39 -0600

    fully working game with scoring

--- app.js
+++ app.js
@@ -1,42 +1,53 @@
 //gets computer's random play
 function computerPlay() {
-    const plays = ["Rock", "Paper", "Scissors"];
-    const play = plays[Math.floor(Math.random() * plays.length)];
-    return play;
+    const cPlays = ["Rock", "Paper", "Scissors"];
+    const cPlay = cPlays[Math.floor(Math.random() * cPlays.length)];
+    return cPlay;
 }
 
 //gets player's inputted play
 function playerPlay() {
-    const play = prompt("What's your play?").toLowerCase();
-    return play.charAt(0).toUpperCase() + play.slice(1);
+    const pPlay = prompt("What's your play?").toLowerCase();
+    return pPlay.charAt(0).toUpperCase() + pPlay.slice(1);
 }
 
 //plays a round and determines winner or tie
-function playRound(playerSelection, computerSelection) {
-    let result = 0;
+function playRound(playerSelection, computerSelection, playerTally, computerTally) {
+    let result;
     if ( (playerSelection == "Rock" && computerSelection == "Scissors") || 
     (playerSelection == "Scissors" && computerSelection == "Paper") || 
     (playerSelection == "Paper" && computerSelection == "Rock") ) {
         result = "You Win! " + playerSelection + " beats " + computerSelection;
+        playerTally++;
     } else if ( (playerSelection == "Scissors" && computerSelection == "Rock") || 
     (playerSelection == "Paper" && computerSelection == "Scissors") || 
     (playerSelection == "Rock" && computerSelection == "Paper") ) {
         result = "You Lose! " + computerSelection + " beats " + playerSelection;
+        computerTally++;
     } else {
         result = "Tie! You both chose " + playerSelection;
     }
-    return result;
+    return {result, playerTally, computerTally};
 }
 
 //plays five rounds and keeps score
 function game() {
+    let playerTally = 0;
+    let computerTally = 0;
     for (let i = 0; i < 5; i++) {
-        // your code here!
+        const computerSelection = computerPlay();
+        const playerSelection = playerPlay();
+        //console.log("Computer: " + computerSelection);
+        //console.log("Player: " + playerSelection);
+        let results = playRound(playerSelection, computerSelection, playerTally, computerTally);
+        let result = results.result;
+        playerTally = results.playerTally;
+        computerTally = results.computerTally;
+        console.log(result);
+        console.log("Player score: " + playerTally);
+        console.log("Computer score: " + computerTally);
     }
+    console.log(`Game over!\n-Final Score-\nPlayer: ${playerTally}\nComputer: ${computerTally}`);
 }
 
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
-console.log("Computer: " + computerSelection);
-console.log("Player: " + playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+game();
